97a804ff6dbb901e625da52da5155c7956c58ab3
Sonu <sbamniya23@gmail.com>
2018-05-06 15:42:09 +0530

    minor changes

--- server/config/config.js
+++ server/config/config.js
@@ -2,13 +2,17 @@ var nodemailer = require('nodemailer');
 const smtpTransport = require('nodemailer-smtp-transport');
 const env = 'staging';
 var host  = 'mongodb://heroku_756c5xzq:bamniya143@ds115340.mlab.com:15340/heroku_756c5xzq'
+var dbuser = 'heroku_756c5xzq';
+var dbpass = 'bamniya143';
 if (env=='dev') {
     host = 'mongodb://localhost:27017/edu-monit-dev';
+    dbuser = 'root';
+    dbpass = '';
 }
 const db = {
     host: host,
-    user: 'root',
-    password: ''
+    user: dbuser,
+    password: dbpass
 }
 const user = 'sbamniya23@gmail.com';
 const pass = 'Bamniya143';
